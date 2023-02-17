1b2e3e490225aecc4fd9350bbc55551369a4cc04
Md. Alamin Ridoy <105815589+Alaminswe@users.noreply.github.com>
2023-02-18 02:47:16 +0600

    recursion

---
+++ week-8/Module-28-More About pointer/malloc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int a[n];
+
+    int *ptr;
+    ptr = (int *)malloc(sizeof(n * int));
+
+    return 0;
+}
